32813b6cea56b4b64b6da71b74abbe98b08211c7
czx <yzcheng90@qq.com>
2024-12-03 10:55:13 +0800

    调整代码

--- src/main/java/com/suke/czx/Application.java
+++ src/main/java/com/suke/czx/Application.java
@@ -3,10 +3,12 @@ package com.suke.czx;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class Application {
 
