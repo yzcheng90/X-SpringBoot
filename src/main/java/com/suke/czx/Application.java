4ff50a16c62fc8a977d1e6e2dd26cf3f8a4687e6
czx <yzcheng90@qq.com>
2024-12-03 10:50:27 +0800

    add @EnableAsync

--- src/main/java/com/suke/czx/Application.java
+++ src/main/java/com/suke/czx/Application.java
@@ -3,8 +3,10 @@ package com.suke.czx;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
+@EnableAsync
 @SpringBootApplication
 public class Application {
 
